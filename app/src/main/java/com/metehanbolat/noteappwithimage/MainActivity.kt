d6d5cf19f9834e2288f83ba2edfbe50f8fa3b0b8
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-20 23:51:31 +0300

    User permission for gallery and convert uri to bitmap

--- app/src/main/java/com/metehanbolat/noteappwithimage/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/noteappwithimage/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.metehanbolat.noteappwithimage
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import com.google.android.material.snackbar.Snackbar
 import com.metehanbolat.noteappwithimage.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture : Bitmap? = null
 
     private val adapter by lazy { MyAdapter() }
     private lateinit var myViewModel: MyViewModel
@@ -27,28 +43,73 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        registerLauncher()
+
         myViewModel = ViewModelProvider(this)[MyViewModel::class.java]
 
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
-        lifecycleScope.launch {
-            val person = Person("Metahan", "Bolat", getBitmap())
-            myViewModel.insertPerson(person)
-        }
-
         myViewModel.readPerson.observe(this) {
             adapter.setData(it)
         }
 
+        binding.selectImage.setOnLongClickListener {
+            selectedPicture?.let { bitmap ->
+                val person = Person("Metahan", "Bolat", bitmap)
+                myViewModel.insertPerson(person)
+            }
+            true
+        }
+
+        binding.selectImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Snackbar.make(it, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }.show()
+                }else {
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }else {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }
+
     }
 
-    private suspend fun getBitmap(): Bitmap {
-        val loading = ImageLoader(this)
-        val request = ImageRequest.Builder(this)
-            .data("https://i.hizliresim.com/7tt0f1t.png")
-            .build()
-        val result = (loading.execute(request) as SuccessResult).drawable
-        return (result as BitmapDrawable).bitmap
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if (intentFromResult != null) {
+                    val imageData = intentFromResult.data
+                    if (imageData != null) {
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(contentResolver, imageData)
+                                selectedPicture = ImageDecoder.decodeBitmap(source)
+                                binding.selectImage.setImageBitmap(selectedPicture)
+                            }else {
+                                selectedPicture = MediaStore.Images.Media.getBitmap(contentResolver, imageData)
+                                binding.selectImage.setImageBitmap(selectedPicture)
+                            }
+                        }catch (e: Exception) {
+                            Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else {
+                Toast.makeText(this, "Permission Needed!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
